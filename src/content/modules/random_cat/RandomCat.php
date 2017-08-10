7c83e9447f7f98a804138fdb05741dfba00ed0ca
Ulrich Schmidt <admin@deruli.de>
2017-08-10 20:05:19 +0200

    erster commit

---
+++ src/content/modules/random_cat/RandomCat.php
@@ -0,0 +1,14 @@
+<?php
+class RandomCat extends Controller {
+	public function render() {
+		$cat = '<div class="random-cat">';
+		$cat .= '<a href="http://thecatapi.com"><img src="http://thecatapi.com/api/images/get?format=src&type=gif"></a>';
+		$cat .= '</div>';
+		$cat = apply_filter ( $cat, 'random_cat' );
+		return $cat;
+	}
+	public function accordionLayout() {
+		echo '<h2 class="accordion-header">' . get_translation ( "cat" ) . '</h2>
+<div class="accordion-content">' . $this->render () . '</div>';
+	}
+}
