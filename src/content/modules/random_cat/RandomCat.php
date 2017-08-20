ef61fb722048558c6d4773123c47388babfcc980
Ulrich Schmidt <admin@deruli.de>
2017-08-20 13:45:12 +0200

    Katzenbilder sind nun responsive

--- src/content/modules/random_cat/RandomCat.php
+++ src/content/modules/random_cat/RandomCat.php
@@ -1,14 +1,20 @@
 <?php
-class RandomCat extends Controller {
-	public function render() {
-		$cat = '<div class="random-cat">';
-		$cat .= '<a href="http://thecatapi.com"><img src="http://thecatapi.com/api/images/get?format=src&type=gif"></a>';
-		$cat .= '</div>';
-		$cat = apply_filter ( $cat, 'random_cat' );
-		return $cat;
-	}
-	public function accordionLayout() {
-		echo '<h2 class="accordion-header">' . get_translation ( "cat" ) . '</h2>
-<div class="accordion-content">' . $this->render () . '</div>';
-	}
+
+class RandomCat extends Controller
+{
+
+    public function render()
+    {
+        $cat = '<div class="random-cat">';
+        $cat .= '<a href="http://thecatapi.com"><img class=\"responsive-image\" src="http://thecatapi.com/api/images/get?format=src&type=gif"></a>';
+        $cat .= '</div>';
+        $cat = apply_filter($cat, 'random_cat');
+        return $cat;
+    }
+
+    public function accordionLayout()
+    {
+        echo '<h2 class="accordion-header">' . get_translation("cat") . '</h2>
+<div class="accordion-content">' . $this->render() . '</div>';
+    }
 }
